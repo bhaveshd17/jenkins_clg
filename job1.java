2471500d47d2cc2077301910d8b860e9e7b965db
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-10-26 21:14:02 +0530

    java

---
+++ job1.java
@@ -0,0 +1,5 @@
+public class job1 {
+    public static void main(String[] args) {
+        System.out.println("Job 1");
+    }
+}
---
+++ job2.java
@@ -0,0 +1,5 @@
+public class job2 {
+    public static void main(String[] args) {
+        System.out.println("Job 2");
+    }
+}
